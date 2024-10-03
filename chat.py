8066483b4c43ece559ec86c6fc96bee6355b7ec6
Tun Hein <tminhein@gmail.com>
2024-10-02 22:43:30 -0700

    add chat

--- chat.py
+++ chat.py
@@ -2,7 +2,7 @@ from ctransformers import AutoModelForCausalLM
 
 llm = AutoModelForCausalLM.from_pretrained("zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf")
 
-prompt = """What is the name of the capital city of India, she asked.
+prompt = """What is the best university in the world, she asked.
           Please only respond with the city name and then stop talking. 
           He answered: """
 
